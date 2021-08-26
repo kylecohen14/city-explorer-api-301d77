6bbc02bcdf05c3ea46d24dd9ea4dff87d6e89ffd
kylec <KYLECOHEN14@GMAIL.COM>
2021-08-25 20:07:20 -0400

    weather forecast working from hardcode data

--- server.js
+++ server.js
@@ -9,37 +9,44 @@ const app = express();
 
 dotenv.config();
 app.use(cors());
-// do i need use(express.json)?
-
-
 
 const PORT = process.env.PORT || 3001;
+
 
 app.get('/weather', (req, res) => {
   console.log(req.query);
   const lat = req.query.lat;
   const lon =req.query.lon;
   const searchQuery = req.query.searchQuery;
-  const city = new Forecast(weatherData.find (city => city_name.toLowerCase() === searchQuery.toLowerCase()))
+  const cityObject = weatherData.find (city => city.city_name.toLowerCase() === searchQuery.toLowerCase());
 
-  class Forecast {
-    constructor(description, date) {
-      this.description = description;
-      this.date = date;
-      forecastArray = []
-    }
+  try{
+    const forecast = cityObject.data.map(day => new Forecast(day));
+    res.send(forecast);
+  }catch(error){
+    console.log(error);
+    notFoundHandler();
   }
-  // res(200).send(city.forcastArray);
-  // res.send([lat, lon, searchQuery]);
-  let forecastArray = [];
-  city.data.map( (value, idx) => {
-    forecastArray.push(new Forecast(value.datetime, `Low of ${value.low_temp}, high of ${value.high_temp}, with ${value.weather.description}`));
-  })
-  res.send(forecastArray);
-});
 
-app.get('*', (req, res) => {
-  res.status(404).send('not found');
 });
+class Forecast {
+  constructor(day) {
+    this.date = day.valid_date;
+    this.description = day.weather.description;
+    this.low_temp = day.low_temp;
+    this.max_temp = day.max_temp;
+  }
+}
+
+
+app.get('*', notFoundHandler);
+
+function notFoundHandler(req, res) {
+  res.status(500).send('not here! Go somewhere else 500')
+}
+
+// app.get('*', (req, res) => {
+//   res.status(500).send('not found 500');
+// });
 
 app.listen(PORT, () => console.log(`listening port ${PORT}`))
